55b451ac809ab51a5ef0b84d244c0b009ea56a1c
Hans Queja <hansqueja8@gmail.com>
2025-05-23 23:28:28 +0800

    Add main file for running the program

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from helpers.helpers import load_map
+
+# Runs the whole program
+def main():
+    input_map = load_map()
+    print("This is the main program!")
+    print(input_map)
+
+
+if __name__ == "__main__":
+    main()
